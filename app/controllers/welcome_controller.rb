09e34172945a9f4c50e6ce5e258b1c56c0b47985
musicprogram <juan-o-henao@hotmail.es>
2014-09-11 18:14:04 -0500

    navbar funcional

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,19 @@
+class WelcomeController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def features
+  end
+
+  def pricing
+  end
+end
